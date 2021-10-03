c75609ecd15ada8e85b0e57fa57d91da38bc27e1
Yuri Gonchar <yuri.gonchar@hotmail.com>
2021-10-03 22:09:05 +0300

    TreeViewSort with line printing

--- TreeViewSort/Program.cs
+++ TreeViewSort/Program.cs
@@ -9,8 +9,6 @@ namespace TreeViewSort
         {
             int[] input = new int[15] { 1, 15, 14, 13, 11, 10, 12, 5, 9, 2, 6, 3, 8, 7, 4 };
 
-            Console.WriteLine("Hello World!");
-
             //SiftingIsCarriedOut(input);
             HeapSort.BasePrintArray(input);
             HeapSort.Sorting(input);
@@ -25,17 +23,26 @@ namespace TreeViewSort
         public static void Sorting(int[] input)
         {
             int inputLength = input.Length;
+
+            Console.WriteLine($"\n ------------------------ Part 1 - Building the heap (rearranging the array) ------------------------");
+
+            // Построение кучи (перегруппируем массив)
             for (int i = (inputLength / 2 - 1); i >= 0; i--)
             {
                 Heapify(input, inputLength, i);
             }
 
+            Console.WriteLine($"\n ++++++++++++++++++++++++ Part 2 - Fetch elements from the heap one by one ++++++++++++++++++++++++");
+
+            // Один за другим извлекаем элементы из кучи
             for (int i = inputLength - 1; i >= 0; i--)
             {
-                //int temp = input[0];
-                //input[0] = input[i];
-                //input[i] = temp;
+                // Перемещаем текущий корень в конец
+                Console.WriteLine($"\nMove the current root from [{0}] to the end - [{i}]");
+                Console.WriteLine($"Start = {input[0]}, End = {input[i]}");
                 (input[0], input[i]) = (input[i], input[0]);
+                Console.WriteLine($"Start = {input[0]}, End = {input[i]}\n");
+
                 Heapify(input, i, 0);
             }
         }
@@ -48,28 +55,30 @@ namespace TreeViewSort
             int left = 2 * i + 1;
             int right = 2 * i + 2;
 
-            Console.WriteLine($"largest = {largest}, left = {largest}, right = {right}");
-            HeapSort.PrintArrayWithRoot(input, i);
+            Console.WriteLine($"input largest[{largest}] = {input[largest]}");
+            HeapSort.PrintArrayWithRoot(input, largest);
 
             bool leftMoreRoot = left.IsOutOfIndexIn(inputLength) && input[left] > input[largest]; // left < inputLength 
             if (leftMoreRoot)
             {
                 largest = left;
+                Console.WriteLine($"input left[{left}] = {input[left]}");
+                HeapSort.PrintArrayWithRoot(input, largest);
             }
 
             bool rightMoreLargest = right.IsOutOfIndexIn(inputLength) && input[right] > input[largest];
             if (rightMoreLargest)
             {
                 largest = right;
+                Console.WriteLine($"input right[{right}] = {input[right]}");
+                HeapSort.PrintArrayWithRoot(input, largest);
             }
 
             if (largest != i)
             {
-                //int swap = input[i];
-                //input[i] = input[largest];
-                //input[largest] = swap;
                 (input[i], input[largest]) = (input[largest], input[i]);
 
+                HeapSort.PrintArrayWithRoot(input, largest);
                 // Рекурсивно преобразуем в двоичную кучу затронутое поддерево
                 Heapify(input, inputLength, largest);
             }
@@ -79,10 +88,10 @@ namespace TreeViewSort
         {
             BasePrintArray(input, $" root = {root}");
         }
-        
+
         public static void BasePrintArray(int[] input, string additions = "")
         {
-            int inputLength = input.Length;            
+            int inputLength = input.Length;
 
             string currentIsCurrent = String.Equals(additions, "") ? "" : $" / {additions}";
 
